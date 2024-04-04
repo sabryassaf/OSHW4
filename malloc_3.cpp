2b636ea56c5e0f2ade6f6d8c5aaef42adab02fa5
sabryassaf <sabry.assaf12@gmail.com>
2024-04-04 02:23:11 +0000

    began part 3

---
+++ malloc_3.cpp
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <math.h>
+
+#define MAX_ORDER 10
+
+// initiate global variables to track free blocks,bytes and alocated blocks,bytes
+size_t freeBlocks = 0;
+size_t freeBytes = 0;
+size_t allocatedBlocks = 0;
+size_t allocatedBytes = 0;
+size_t metadataBytes = 0;
+
+
+// MallocMetaData
+struct MallocMetaData {
+    size_t size;
+    bool is_free;
+    MallocMetaData* next;
+    MallocMetaData* prev;
+};
+
+// global trackers
+MallocMetaData* listOfOrders[MAX_ORDER];
+bool flag = false;
+
+void* smalloc(size_t size) {
+    // first usage of smalloc, allocate the list of orders
+    if(!flag) {
+        flag = true;
+        for(int i = 0; i < MAX_ORDER; i++) {
+            listOfOrders[i] = NULL;
+        }
+    // alligh the initial 32 blocks of size 128 kb in memoery so their starting address is multiple of 32*128kb
+    
+
+    }
+
+
+}
